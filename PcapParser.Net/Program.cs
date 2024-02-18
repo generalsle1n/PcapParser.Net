7c37b07bbd816567dcf3b252bdaffe30964333e5
generalsle1n <niels.schuler@web.de>
2024-02-18 09:42:35 +0100

    Configured Commandline

--- PcapParser.Net/Program.cs
+++ PcapParser.Net/Program.cs
@@ -5,9 +5,12 @@ RootCommand Root = new RootCommand();
 
 Command GetDistinct = new Command(name: "--Distinct", description: "Get distinct LAN to WAN Access");
 Option<string[]> LanFilter = new Option<string[]>(name: "--LanFilter", description: "How to Specify Lan");
-LanFilter.SetDefaultValue(new string[] { "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16" });
+LanFilter.SetDefaultValue(new string[] { "src net 10.0.0.0/8", "src net 172.16.0.0/12", "src net 192.168.0.0/16" });
 
-Option<string> InputFile = new Option<string>(name: "--File", description: "Enter File");
+Option<string> InputFile = new Option<string>(name: "--File", description: "Enter File")
+{
+    IsRequired = true
+};
 
 GetDistinct.AddOption(LanFilter);
 GetDistinct.AddOption(InputFile);
@@ -16,16 +19,12 @@ GetDistinct.SetHandler((InputLanFilter, InputFile) =>
 {
     PcapAnalyzer Pcap = new PcapAnalyzer()
     {
-        InputFile = InputFile
+        InputFile = InputFile,
+        Filter = InputLanFilter
     };
+    Pcap.ParsePcap();
 }, LanFilter, InputFile);
 
 Root.AddCommand(GetDistinct);
-
-PcapAnalyzer Pcap = new PcapAnalyzer()
-{
-    InputFile = "/home/niels/Schreibtisch/Data.pcap.ng"
-};
-Pcap.ParsePcap();
 
 await Root.InvokeAsync(args);
