d6c916315246c61ae808518b24d3012c2b735f2a
generalsle1n <niels.schuler@web.de>
2024-02-18 20:02:45 +0100

    Added Nuget

--- PcapParser.Net/PcapAnalyzer.cs
+++ PcapParser.Net/PcapAnalyzer.cs
@@ -3,6 +3,7 @@ using SharpPcap;
 using SharpPcap.LibPcap;
 using PacketDotNet;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace PcapParser.Net;
 
@@ -10,33 +11,60 @@ public class PcapAnalyzer
 {
     public string InputFile { get; init; }
     public string[] Filter { get; set; }
+    public string Exclude { get; set; }
     private const string _orFilter = "or";
+    private const string _andFilter = "and";
     private const string _whitespace = " ";
 
     private string CreateFilterText()
     {
         StringBuilder Builder = new StringBuilder();
-
+        Builder.Append("(");
         foreach (string expression in Filter)
         {
             Builder.Append(expression).Append(_whitespace).Append(_orFilter).Append(_whitespace);
         }
-        Builder.Remove(Builder.Length - 4, 4);
+        Builder.Remove(Builder.Length - 4, 4).Append(")");
+
+        if (Exclude is not null)
+        {
+            Builder.Append(_andFilter).Append("(").Append(Exclude).Append(")");
+        }
+
 
         return Builder.ToString();
     }
 
-    public void ParsePcap()
+    public void PrintFlow(List<NetFlow> Flow)
+    {
+        foreach (NetFlow SingleFlow in Flow)
+        {
+            Console.WriteLine($"{SingleFlow.Type}:{SingleFlow.Source} --> {SingleFlow.Destination}:{SingleFlow.DestinationPort}");
+        }
+    }
+
+    public List<NetFlow> ParsePcap()
     {
         using (CaptureFileReaderDevice Reader = new CaptureFileReaderDevice(InputFile))
         {
+            List<NetFlow> AllFlows = new List<NetFlow>();
+
             PacketCapture Cap;
             Reader.Open(DeviceModes.None);
             Reader.Filter = CreateFilterText();
             while (true)
             {
                 Reader.GetNextPacket(out Cap);
-                RawCapture Raw = Cap.GetPacket();
+                RawCapture Raw;
+                try
+                {
+                    Raw = Cap.GetPacket();
+                }
+                catch (NullReferenceException ex)
+                {
+                    break;
+                }
+
                 Packet LinkLayer = Packet.ParsePacket(Raw.LinkLayerType, Raw.Data);
                 if (LinkLayer.PayloadPacket is IPv4Packet)
                 {
@@ -44,15 +72,55 @@ public class PcapAnalyzer
                     if (Packet.PayloadPacket is TcpPacket)
                     {
                         TcpPacket TcpPacket = (TcpPacket)Packet.PayloadPacket;
-                        Console.WriteLine($"{Packet.SourceAddress} --> {Packet.DestinationAddress}:{TcpPacket.DestinationPort}");
+                        bool NotExists = AllFlows.Where(flow => flow.Source.Equals(Packet.SourceAddress) && flow.Destination.Equals(Packet.DestinationAddress) && flow.DestinationPort == TcpPacket.DestinationPort).Count() == 0;
+
+                        if (NotExists == true)
+                        {
+                            AllFlows.Add(new NetFlow()
+                            {
+                                Source = Packet.SourceAddress,
+                                Destination = Packet.DestinationAddress,
+                                DestinationPort = TcpPacket.DestinationPort,
+                                Type = NetFlowType.TCP
+                            });
+                        }
                     }
                     else if (Packet.PayloadPacket is UdpPacket)
                     {
                         UdpPacket UdpPacket = (UdpPacket)Packet.PayloadPacket;
-                        Console.WriteLine($"{Packet.SourceAddress} --> {Packet.DestinationAddress}:{UdpPacket.DestinationPort}");
+                        bool NotExists = AllFlows.Where(flow => flow.Source.Equals(Packet.SourceAddress) && flow.Destination.Equals(Packet.DestinationAddress) && flow.DestinationPort == UdpPacket.DestinationPort).Count() == 0;
+
+                        if (NotExists == true)
+                        {
+                            AllFlows.Add(new NetFlow()
+                            {
+                                Source = Packet.SourceAddress,
+                                Destination = Packet.DestinationAddress,
+                                DestinationPort = UdpPacket.DestinationPort,
+                                Type = NetFlowType.UDP
+                            });
+                        }
+                    }
+                    else if (Packet.PayloadPacket is IcmpV4Packet)
+                    {
+                        IcmpV4Packet IcmpPacket = (IcmpV4Packet)Packet.PayloadPacket;
+                        bool NotExists = AllFlows.Where(flow => flow.Source.Equals(Packet.SourceAddress) && flow.Destination.Equals(Packet.DestinationAddress) && flow.Type == NetFlowType.ICMP).Count() == 0;
+
+                        if (NotExists == true)
+                        {
+                            AllFlows.Add(new NetFlow()
+                            {
+                                Source = Packet.SourceAddress,
+                                Destination = Packet.DestinationAddress,
+                                DestinationPort = 0,
+                                Type = NetFlowType.ICMP
+                            });
+                        }
                     }
                 }
             }
+
+            return AllFlows;
         }
     }
 }
@@ -63,10 +131,4 @@ public class NetFlow
     public IPAddress Destination { get; set; }
     public int DestinationPort { get; set; }
     public NetFlowType Type { get; set; }
-}
-
-public enum NetFlowType
-{
-    TCP,
-    UDP
 }
