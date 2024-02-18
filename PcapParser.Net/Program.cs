5ec0ba0262e4b5f82f9d228c1f10c0d9e4479297
generalsle1n <niels.schuler@web.de>
2024-02-18 20:02:53 +0100

    Added Exporter

--- PcapParser.Net/Program.cs
+++ PcapParser.Net/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.CommandLine;
+using System.Globalization;
+using System.Text.Json;
+using CsvHelper;
 using PcapParser.Net;
 
 RootCommand Root = new RootCommand();
@@ -12,18 +15,53 @@ Option<string> InputFile = new Option<string>(name: "--File", description: "Ente
     IsRequired = true
 };
 
+Option<string> ExcludeFilter = new Option<string>(name: "--Exclude", description: "Enter Filter to Exclude (PcapFilter Expression: src host not 192.168.1.1)");
+Option<string> OutputPath = new Option<string>(name: "--Output", description: "Enter the Path where the File should be saved")
+{
+    IsHidden = true
+};
+Option<SaveType> SaveTypeFormat = new Option<SaveType>(name: "--Type", description: "Enter the Type in which foramt the output should be saved")
+{
+    IsHidden = true
+};
+SaveTypeFormat.SetDefaultValue(SaveType.JSON);
+
 GetDistinct.AddOption(LanFilter);
 GetDistinct.AddOption(InputFile);
+GetDistinct.AddOption(ExcludeFilter);
+GetDistinct.AddOption(OutputPath);
+GetDistinct.AddOption(SaveTypeFormat);
 
-GetDistinct.SetHandler((InputLanFilter, InputFile) =>
+
+GetDistinct.SetHandler((InputLanFilter, InputFile, InputExclude, InputOutPath, InputSaveTypeFormat) =>
 {
     PcapAnalyzer Pcap = new PcapAnalyzer()
     {
         InputFile = InputFile,
-        Filter = InputLanFilter
+        Filter = InputLanFilter,
+        Exclude = InputExclude
     };
-    Pcap.ParsePcap();
-}, LanFilter, InputFile);
+    List<NetFlow> Flow = Pcap.ParsePcap();
+
+    if (InputOutPath is null)
+    {
+        Pcap.PrintFlow(Flow);
+    }
+    else if (InputSaveTypeFormat == SaveType.JSON)
+    {
+        string Content = JsonSerializer.Serialize(Flow);
+        File.WriteAllText(InputOutPath, Content);
+    }
+    else if (InputSaveTypeFormat == SaveType.CSV)
+    {
+        using (StreamWriter SWriter = new StreamWriter(InputOutPath))
+        using (CsvWriter CWriter = new CsvWriter(SWriter, CultureInfo.InvariantCulture))
+        {
+            CWriter.WriteRecords(Flow);
+        }
+    }
+
+}, LanFilter, InputFile, ExcludeFilter, OutputPath, SaveTypeFormat);
 
 Root.AddCommand(GetDistinct);
 
